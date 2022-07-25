c1d0f46ec24ab77ec14a15359ebb5b74e01efda2
C PremSai <saiprem2001@gmail.com>
2022-07-25 18:34:53 +0000

    updated

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -33,26 +33,3 @@ async def test_seq_bug1(dut):
     await Timer(2, units='ns')
 
     assert dut.seq_seen.value == 1, "Sequence is pesent but not Detected" 
-
-@cocotb.test()
-async def randomized_test_seq_bug1(dut):
-    """Test for seq detection """
-
-    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
-    cocotb.start_soon(clock.start())        # Start the clock
-
-    # reset
-    dut.reset.value = 1
-    await FallingEdge(dut.clk)  
-    dut.reset.value = 0
-    await FallingEdge(dut.clk)
-
-    cocotb.log.info('#### CTB: Develop your test here! ######')
-    for i in range(10):
-        in_bit = random.randint(0,1)
-        print(in_bit)
-        dut.inp_bit.value = in_bit
-
-    await Timer(2, units='ns')
-
-    assert dut.seq_seen.value == 1, "Sequence is pesent but not Detected" 
